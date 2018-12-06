40705c7173d5513ed4f7a15d9d42c42147ac3620
seenevz <serneves@hotmail.com>
2018-12-06 23:15:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  return_a = []
+
+  while i < collection.length do
+        if yield(collection[i]) == true
+          return_a.push(collection[i])
+        end
+        i += 1
+  end
+  return_a
 end
